ccfb3f7ebae090263da020feb6f87b33bc313946
P0Ci <febriandry58@gmail.com>
2025-02-18 21:55:33 +0700

    Completed Controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,10 +2,116 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //get all posts
+        $posts = Post::latest()->paginate(5);
+
+        //return collection of posts as a resource
+        return new PostResource(true, 'List Data Posts', $posts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        $post = Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan', $post);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::find($id);
+
+        return new PostResource(true, 'Detail Data Post', $post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //define validation
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $post = Post::find($id);
+
+        if ($request->hasFile('image')) {
+
+            $image = $request->file('image');
+            $image->storeAs('public/posts/', $image->hashName());
+
+            Storage::delete('public/posts/'.basename($post->image));
+
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        } else {
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        }
+
+        return new PostResource(true, 'Data Post Berhasil Diubah', $post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::find($id);
+
+        Storage::delete('public/posts/'.basename($post->image));
+
+        $post->delete();
+
+        return new PostResource(true, 'Data Post Berhasil Di Hapus', null);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('api')->group(function () {
+    Route::withoutMiddleware([VerifyCsrfToken::class])->group(function () {
+        Route::apiResource('posts', PostController::class);
+    });
+});
+
