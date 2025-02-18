77269b364471b849adb7cae891a5e7e8886366df
P0Ci <febriandry58@gmail.com>
2025-02-18 09:14:47 +0700

    Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use App\Http\Resources\PostResource;
+
+
+class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::latest()->paginate(5);
+        return new PostResource(true, 'List Data Posts', $posts);
+    }
 }
